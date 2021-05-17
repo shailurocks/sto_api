c948d2a62b68424b0e1566f22912a0f4cce0bc97
unknown <shailesh.p@purplle.com>
2021-05-17 15:48:54 +0530

    installed sequalize , sequalize-typescript , mysql2 & DB connection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,38 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  Sequelize,
+  Model,
+  ModelDefined,
+  DataTypes,
+  HasManyGetAssociationsMixin,
+  HasManyAddAssociationMixin,
+  HasManyHasAssociationMixin,
+  Association,
+  HasManyCountAssociationsMixin,
+  HasManyCreateAssociationMixin,
+  Optional,
+  QueryTypes
+} from "sequelize";
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  
+  constructor(
+              private readonly appService: AppService
+            ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    const db_connection = new Sequelize("mysql://web_user:36dXUwiaflxKKjposE90@10.140.0.95:3306/purplle_purplle2");
+    
+    let query:string = "SELECT id from `procurement_po` limit 1";
+    let queryParams :Object = {};
+
+    const dbResults = await db_connection.query(query, { type: QueryTypes.SELECT });
+    //this.sequelizeInstance.query(query, { type: QueryTypes.SELECT, replacements:replacements });
+    console.log(dbResults , "dbResults")
+    //return this.appService.getHello();
   }
 }
