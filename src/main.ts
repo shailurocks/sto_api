f577273a503a718f80cb9adfaf40c84ce41448c1
unknown <shailesh.p@purplle.com>
2021-05-17 14:43:14 +0530

    added environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-
+import * as dotenv from "dotenv";
+dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
